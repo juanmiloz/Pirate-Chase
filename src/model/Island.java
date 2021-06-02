d36c36d85430eb2852722a8c8a332dc1861c39fe
Juanserodriguez16 <juseros9@gmail.com>
2021-06-01 22:56:22 -0500

    coments of model package

--- src/model/Island.java
+++ src/model/Island.java
@@ -12,7 +12,16 @@ public class Island {
 	private boolean adyacent;
 	private boolean energy;
 	private PApplet app;
-	
+	/**
+     * Name: Island
+     * Constructor method of a Island. <br>
+	 * @param posX - Position in the map - posX = int
+	 * @param posY - Position in the map - posY = int
+	 * @param occupied - If island stay occupied - occupied = boolean
+	 * @param adyacent - If island is adyacent - adyacent = boolean
+	 * @param islandNumber - number of island - islandNumber = int
+	 * @param app - PApplet
+	 */
 	public Island(int posX, int posY, boolean occupied, boolean adyacent,int islandNumber,PApplet app) {
 		this.app=app;
 		islandLight= app.loadImage("images/island.png");
@@ -26,7 +35,15 @@ public class Island {
 		this.occupied=occupied;
 		this.islandNumber=islandNumber;
 	}
-	
+	/**
+	 * 
+     * Name: Island
+     * Constructor method of a Island. <br>
+	 * @param posX - Position in the map - posX = int
+	 * @param posY - Position in the map - posY = int
+	 * @param islandNumber - number of island - islandNumber = int
+	 * @param app - PApplet
+	 */
 	public Island(int posX, int posY,int islandNumber,PApplet app) {
 		this.app=app;
 		islandLight= app.loadImage("images/island.png");
@@ -40,7 +57,14 @@ public class Island {
 		occupied=false;
 		this.islandNumber=islandNumber;
 	}
-	
+	/**
+	 * 
+     * Name: Island
+     * Constructor method of a Island. <br>
+	 * @param posX - Position in the map - posX = int
+	 * @param posY - Position in the map - posY = int
+	 * @param islandNumber - number of island - islandNumber = int
+	 */
 	public Island(int posX, int posY, int islandNumber) {
 		this.posX = posX;
 		this.posY = posY;
@@ -53,7 +77,10 @@ public class Island {
 	
 	
 	
-	
+	/**
+     * Name: drawIsland
+     * Method to draw a new island. <br>
+	 */
 	public void drawIsland() {
 		if(adyacent || occupied) {
 			app.image(islandLight, posX, posY,width,height);
@@ -69,67 +96,131 @@ public class Island {
 	}
 	
 	
-
+	/**
+	 * Name: getPosX
+	 * Method used to get the position of the island. <br>
+	 * @return A Int representing the position of the island.
+	*/
 	public int getPosX() {
 		return posX;
 	}
-
+	/**
+	 * Name: setPosX
+	 * Method used to update the position of island.  <br>
+	 * @param posX - Position in the map - posX = int
+	*/
 	public void setPosX(int posX) {
 		this.posX = posX;
 	}
-
+	/**
+	 * Name: getPosY
+	 * Method used to get the position of the island. <br>
+	 * @return A Int representing the position of the island.
+	*/
 	public int getPosY() {
 		return posY;
 	}
-
+	/**
+	 * Name: setPosY
+	 * Method used to update the position of island.  <br>
+	 * @param posY - Position in the map - posY = int
+	*/
 	public void setPosY(int posY) {
 		this.posY = posY;
 	}
-
+	/**
+	 * Name: getWidth
+	 * Method used to get the width of the island. <br>
+	 * @return A Int representing the width of the island.
+	*/
 	public int getWidth() {
 		return width;
 	}
-
+	/**
+	 * Name: setWidth
+	 * Method used to update the width of island.  <br>
+	 * @param width - width in the map - width = int
+	*/
 	public void setWidth(int width) {
 		this.width = width;
 	}
-
+	/**
+	 * Name: getHeight
+	 * Method used to get the height of the island. <br>
+	 * @return A Int representing the height of the island.
+	*/
 	public int getHeight() {
 		return height;
 	}
-
+	/**
+	 * Name: setHeight
+	 * Method used to update the height of island.  <br>
+	 * @param height - height in the map - height = int
+	*/
 	public void setHeight(int height) {
 		this.height = height;
 	}
-
+	/**
+	 * Name: isOccupied
+	 * Method used to get if is occupied the island. <br>
+	 * @return A boolean representing the occupied of the island.
+	*/
 	public boolean isOccupied() {
 		return occupied;
 	}
-
+	/**
+	 * Name: setOccupied
+	 * Method used to update the position of island.  <br>
+	 * @param occupied - occupied´s map - posY = boolean
+	*/
 	public void setOccupied(boolean occupied) {
 		this.occupied = occupied;
 	}
-
+	/**
+	 * Name: isAdyacent
+	 * Method used to get adyacense of island. <br>
+	 * @return A boolean representing the adyacent of the island.
+	*/
 	public boolean isAdyacent() {
 		return adyacent;
 	}
-
+	/**
+	 * Name: setAdyacent
+	 * Method used to update adyacent of island.  <br>
+	 * @param adyacent - adyacent in the map - adyacent = boolean
+	*/
 	public void setAdyacent(boolean adyacent) {
 		this.adyacent = adyacent;
 	}
-
+	/**
+	 * Name: getIslandNumber
+	 * Method used to get the number of the island. <br>
+	 * @return A Int representing the number of the island.
+	*/
 	public int getIslandNumber() {
 		return islandNumber;
 	}
-
+	/**
+	 * Name: setIslandNumber
+	 * Method used to update the number of island.  <br>
+	 * @param islandNumber - number in the map - islandNumber = int
+	*/
 	public void setIslandNumber(int islandNumber) {
 		this.islandNumber = islandNumber;
 	}
-
+	/**
+	 * Name: isEnergy
+	 * Method used to get if is energy of island. <br>
+	 * @return A boolean representing if is energy of the island.
+	*/
 	public boolean isEnergy() {
 		return energy;
 	}
-
+	/**
+	 * Name: setEnergy
+	 * Method used to update if is energy in island.  <br>
+	 * @param energy - if is energy island - energy = boolean
+	*/
 	public void setEnergy(boolean energy) {
 		this.energy = energy;
 	}
--- src/model/Pirate.java
+++ src/model/Pirate.java
@@ -12,7 +12,10 @@ public class Pirate {
 	private boolean type;
 	private PApplet app;
 	private PImage morgan, user; 
-	
+	/**
+	 * Name: Pirate
+     * Constructor method of a Pirate. <br>
+	 */
 	public Pirate(int posX, int posY, boolean type,PApplet app) {
 		energy = 0;
 		currentIsland = 0;
@@ -38,84 +41,164 @@ public class Pirate {
 		posX = x;
 		posY = y; 
 	}
-	
+	/**
+	 * Name: getEnergy
+	 * Method used to get the energy of the pirate. <br>
+	 * @return A Int representing the energy of the pirate.
+	*/
 	public int getEnergy() {
 		return energy;
 	}
-	
+	/**
+	 * Name: setEnergy
+	 * Method used to update the energy of the pirate.  <br>
+	 * @param energy - energy of pirate - energy = int
+	*/
 	public void setEnergy(int energy) {
 		this.energy = energy;
 	}
-
+	/**
+	 * Name: getCurrentIsland
+	 * Method used to get the current island. <br>
+	 * @return A Int representing the current island.
+	*/
 	public int getCurrentIsland() {
 		return currentIsland;
 	}
-
+	/**
+	 * Name: setCurrentIsland
+	 * Method used to update the current island.  <br>
+	 * @param currentIsland - currentIsland  - currentIsland = int
+	*/
 	public void setCurrentIsland(int currentIsland) {
 		this.currentIsland = currentIsland;
 	}
-
+	/**
+	 * Name: getPosX
+	 * Method used to get the position of the pirate. <br>
+	 * @return A Int representing the position of the pirate.
+	*/
 	public int getPosX() {
 		return posX;
 	}
-
+	/**
+	 * Name: setPosX
+	 * Method used to update the position of pirate.  <br>
+	 * @param posX - Position in the map - posX = int
+	*/
 	public void setPosX(int posX) {
 		this.posX = posX;
 	}
-
+	/**
+	 * Name: getPosY
+	 * Method used to get the position of the pirate. <br>
+	 * @return A Int representing the position of the pirate.
+	*/
 	public int getPosY() {
 		return posY;
 	}
-
+	/**
+	 * Name: setPosY
+	 * Method used to update the position of pirate.  <br>
+	 * @param posY - Position in the map - posY = int
+	*/
 	public void setPosY(int posY) {
 		this.posY = posY;
 	}
-
+	/**
+	 * Name: getWidth
+	 * Method used to get the width of the pirate . <br>
+	 * @return A Int representing the width of the pirate.
+	*/
 	public int getWidth() {
 		return width;
 	}
-
+	/**
+	 * Name: setWidth
+	 * Method used to update the width of pirate.  <br>
+	 * @param width - width in the map - width = int
+	*/
 	public void setWidth(int width) {
 		this.width = width;
 	}
-
+	/**
+	 * Name: getHeight
+	 * Method used to get the height of the pirate. <br>
+	 * @return A Int representing the height of the pirate.
+	*/
 	public int getHeight() {
 		return height;
 	}
-
+	/**
+	 * Name: setHeight
+	 * Method used to update the height of pirate.  <br>
+	 * @param height - height in the map - height = int
+	*/
 	public void setHeight(int height) {
 		this.height = height;
 	}
-
+	/**
+	 * Name: isType
+	 * Method used to get type of the pirate. <br>
+	 * @return A boolean representing the type of the pirate.
+	*/
 	public boolean isType() {
 		return type;
 	}
-
+	/**
+	 * Name: setType
+	 * Method used to update the type of pirate.  <br>
+	 * @param type - type of pirate - type = boolean
+	*/
 	public void setType(boolean type) {
 		this.type = type;
 	}
-
+	/**
+	 * Name: getApp
+	 * Method used to get app of the pirate. <br>
+	 * @return A PApplet representing the app of the pirate.
+	*/
 	public PApplet getApp() {
 		return app;
 	}
-
+	/**
+	 * Name: setApp
+	 * Method used to update the app.  <br>
+	 * @param app - app = PApplet
+	*/
 	public void setApp(PApplet app) {
 		this.app = app;
 	}
-
+	/**
+	 * Name: getMorgan
+	 * Method used to get mprgan´s pirate. <br>
+	 * @return A boolean representing the type of the pirate.
+	*/
 	public PImage getMorgan() {
 		return morgan;
 	}
-
+	/**
+	 * Name: setMorgan
+	 * Method used to update morgan.  <br>
+	 * @param morgan - morgan image - morgan = PImage
+	*/
 	public void setMorgan(PImage morgan) {
 		this.morgan = morgan;
 	}
 
-
+	/**
+	 * Name: getUser
+	 * Method used to get user. <br>
+	 * @return A boolean representing the user.
+	*/
 	public PImage getUser() {
 		return user;
 	}
-
+	/**
+	 * Name: setUser
+	 * Method used to update the user.  <br>
+	 * @param user - PImage
+	*/
 	public void setUser(PImage user) {
 		this.user = user;
 	}
