0ad0baf1585722ee834248f1bba717e93a326335
Giovanni <Giovanni2414g@gmail.com>
2021-06-03 02:03:31 -0500

    Advance

--- test/structures/graphArrayList/GraphArrayTest.java
+++ test/structures/graphArrayList/GraphArrayTest.java
@@ -14,14 +14,12 @@ public class GraphArrayTest {
 		return graph;
 	}
 	
-	
-	void addVertexTest() {
-		GraphArray<Island> graph = setupScenary1();
+	private GraphArray<Island> setupScenary2() {
+		GraphArray<Island> graph = new GraphArray<Island>();
 		Island is1 = new Island(50,50,0);
 		VertexArray<Island> vt1 = graph.createVertex(is1);
 		graph.addVertex(vt1, null, null);
-		assertNotNull(graph.getRoot());
-		Island is2 = new Island(50,50,1);
+		Island is2 = new Island(50,50,0);
 		VertexArray<Island> vt2 = graph.createVertex(is2);
 		ArrayList<VertexArray<Island>> connections1 = new ArrayList<VertexArray<Island>>();
 		connections1.add(vt1);
@@ -32,29 +30,36 @@ public class GraphArrayTest {
 		ArrayList<VertexArray<Island>> connections2 = new ArrayList<VertexArray<Island>>();
 		connections2.add(vt1);
 		connections2.add(vt2);
-		int[] weigths2 = {3,7};
+		int[] weigths2 = {2,3};
 		graph.addVertex(vt3, connections2, weigths2);
+		return graph;
+	}
+	
+	@Test
+	void addVertexTest() {
+		GraphArray<Island> graph = setupScenary1();
+		assertNull(graph.getRoot());
+		Island is1 = new Island(50,50,0);
+		VertexArray<Island> vt1 = graph.createVertex(is1);
+		graph.addVertex(vt1, null, null);
+		assertNotNull(graph.getRoot());
 	}
 	
 	@Test
 	void listOfVertexTest() {
-		GraphArray<Island> graphList = setupScenary1();
-		Island is1 = new Island(50,50,0);
-		VertexArray<Island> vt1 = graphList.createVertex(is1);
-		graphList.addVertex(vt1, null, null);
-		Island is2 = new Island(50,50,0);
-		VertexArray<Island> vt2 = graphList.createVertex(is2);
-		ArrayList<VertexArray<Island>> connections1 = new ArrayList<VertexArray<Island>>();
-		connections1.add(vt1);
-		int[] weigths1 = {1};
-		graphList.addVertex(vt2, connections1, weigths1);
-		Island is3 = new Island(50,50,2);
-		VertexArray<Island> vt3 = graphList.createVertex(is3);
-		ArrayList<VertexArray<Island>> connections2 = new ArrayList<VertexArray<Island>>();
-		connections2.add(vt1);
-		connections2.add(vt2);
-		int[] weigths2 = {2,3};
-		graphList.addVertex(vt3, connections2, weigths2);
+		GraphArray<Island> graphList = setupScenary2();
+		ArrayList<VertexArray<Island>> islands = graphList.getVertexList();
+		assertEquals(3, islands.size());
+	}
+	
+	@Test
+	void adyacencyMatrixTest() {
+		GraphArray<Island> graphList = setupScenary2();
+		int[][] matrix = graphList.getAdjacencyMatrix();
+		assertEquals(2, matrix[0][2]);
+		assertEquals(3, matrix[1][2]);
+		assertEquals(0, matrix[2][2]);
+		assertEquals(1, matrix[0][1]);
 	}
 
 }
